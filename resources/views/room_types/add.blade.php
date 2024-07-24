64d47dc5a329afe5885e058f23bc84694a9952b7
Isha <drashti.kalathiyainfotech@gmail.com>
2024-07-24 16:30:20 +0530

    addblade design changes

--- resources/views/room_types/add.blade.php
+++ resources/views/room_types/add.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.master')
 @section('content')
-<style>
-  input[type="checkbox"] {
+    <style>
+        input[type="checkbox"] {
             width: 20px;
             height: 20px;
-    }
-    .checkbox-item {
-        display: flex;
-        align-items: center;
-        margin-bottom: 10px; /* Space below each item */
-    }
+        }
 
-    .checkbox-item input[type="checkbox"] {
-        margin-right: 8px;
-    }
-    .extra-bed-textbox {
-        margin-bottom: 15px; /* Adjust the spacing between textboxes */
-    }
-</style>
+        .checkbox-item {
+            display: flex;
+            align-items: center;
+            margin-bottom: 10px;
+            /* Space below each item */
+        }
+
+        .checkbox-item input[type="checkbox"] {
+            margin-right: 8px;
+        }
+
+        .extra-bed-textbox {
+            margin-bottom: 15px;
+            /* Adjust the spacing between textboxes */
+        }
+    </style>
 
     {{-- message --}}
     {!! Toastr::message() !!}
@@ -26,11 +30,13 @@
             <div class="page-header">
                 <div class="row align-items-center">
                     <div class="col">
+                        {{-- <h3 class="page-title mt-5">Add Room</h3> --}}
                         <div class="page-title mt-5">{{ isset($roomtype) ? 'Edit Room Type' : 'Add Room Type' }}</div>
                     </div>
                 </div>
             </div>
-            <form action="{{ isset($roomtype) ? '/roomtype/update/' . $roomtype->id : '/roomtype/store' }}" method="POST"  enctype="multipart/form-data">
+            <form action="{{ isset($roomtype) ? '/roomtype/update/' . $roomtype->id : '/roomtype/store' }}" method="POST"
+                enctype="multipart/form-data">
                 @csrf
                 <div class="row">
                     <div class="col-lg-12">
@@ -38,7 +44,8 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label>RoomType Name</label>
-                                        <input type="text" class="form-control @error('room_name') is-invalid @enderror" name="room_name" value="{{ old('room_name', $roomtype->room_name ?? '') }}">
+                                    <input type="text" class="form-control @error('room_name') is-invalid @enderror"
+                                        name="room_name" value="{{ old('room_name', $roomtype->room_name ?? '') }}">
                                     @error('room_name')
                                         <div class="error text-danger">{{ $message }}</div>
                                     @enderror
@@ -47,7 +54,8 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label>Capacity</label>
-                                        <input type="number" class="form-control @error('capacity') is-invalid @enderror" name="capacity" value="{{ old('capacity', $roomtype->capacity ?? '') }}">
+                                    <input type="number" class="form-control @error('capacity') is-invalid @enderror"
+                                        name="capacity" value="{{ old('capacity', $roomtype->capacity ?? '') }}">
                                     @error('capacity')
                                         <div class="error text-danger">{{ $message }}</div>
                                     @enderror
@@ -57,7 +65,9 @@
                             <div class="col-md-12">
                                 <div class="form-group">
                                     <label>Base Price</label>
-                                        <input type="number" id="base_price" class="form-control @error('base_price') is-invalid @enderror" name="base_price" value="{{ old('base_price', $roomtype->base_price ?? '') }}">
+                                    <input type="number" id="base_price"
+                                        class="form-control @error('base_price') is-invalid @enderror" name="base_price"
+                                        value="{{ old('base_price', $roomtype->base_price ?? '') }}">
                                     @error('base_price')
                                         <div class="error text-danger">{{ $message }}</div>
                                     @enderror
@@ -75,58 +85,75 @@
                             </div>
 
                             <div class="col-md-6" id="extra_bed_textbox" style="display: none;">
-                                <p id="total_extra_bed_price_display" style="color:red">Total Extra Price <span id="total_extra_bed_price">0.00</span></p>
+                                <p id="total_extra_bed_price_display" style="color:red">Total Extra Price <span
+                                        id="total_extra_bed_price">0.00</span></p>
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group">
-                                            <label for="per_extra_bed_price">Per Extra Bed Price</label>
-                                            <input type="number" id="per_extra_bed_price" class="form-control" name="per_extra_bed_price" value="{{ old('per_extra_bed_price', $roomtype->per_extra_bed_price ?? '') }}">
+                                        <label for="per_extra_bed_price">Per Extra Bed Price</label>
+                                            <input type="number" id="per_extra_bed_price" class="form-control"
+                                                name="per_extra_bed_price"
+                                                value="{{ old('per_extra_bed_price', $roomtype->per_extra_bed_price ?? '') }}">
                                         </div>
                                     </div>
+
                                     <div class="col-md-6">
                                         <div class="form-group">
-                                            <label for="extra_bed_quantity">Extra Bed Quantity</label>
+                                            <label for="extra_bed_quantity_2">Extra Bed Quantity</label>
                                             <input type="number" id="extra_bed_quantity" class="form-control" name="extra_bed_quantity" value="{{ old('extra_bed_quantity', $roomtype->extra_bed_quantity ?? '') }}">
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
-                        </div>
-
-                        <?php
-                        $selectedAmenities = isset($roomtype) ? explode(",", $roomtype->amenities_id) : [];
-                    ?>
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="row formtype">
-                                    <div class="col-lg-6">
-                                        <label>Amenities</label>
-                                        <fieldset class="checkbox-container">
-                                            <div class="row">
-                                                @foreach($amenities as $amenity)
-                                                    <div class="col-md-4 checkbox-item">
-                                                        <input type="checkbox" id="amenity_{{ $amenity->id }}" name="amenities_id[]" value="{{ $amenity->id }}"
-                                                        @if(in_array($amenity->id, $selectedAmenities)) checked @endif>
-                                                        <label for="amenity_{{ $amenity->id }}">{{ $amenity->name }}</label>
-                                                    </div>
-                                                @endforeach
-                                            </div>
-                                        </fieldset>
-                                    </div>
-                                </div>
-                            </div>
-
-
                             <div class="col-md-12">
                                 <div class="form-group">
                                     <label>Extra Base Price</label>
-                                        <input readonly type="number" class="form-control @error('extra_bed_price') is-invalid @enderror" id="total_base_price" name="extra_bed_price">
+                                    <input readonly type="number"
+                                        class="form-control @error('extra_bed_price') is-invalid @enderror"
+                                        id="total_base_price" name="extra_bed_price">
                                     @error('extra_bed_price')
                                         <div class="error text-danger">{{ $message }}</div>
                                     @enderror
                                 </div>
                             </div>
+
+
+                        </div>
+
+
+                    </div>
+                </div>
+                
+                <?php
+                        $selectedAmenities = isset($roomtype) ? explode(",", $roomtype->amenities_id) : [];
+                    ?>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="row formtype">
+                            <div class="col-md-12">
+                                <label>Amenities</label>
+                                <fieldset class="checkbox-container">
+                                    <div class="row">
+                                        @foreach($amenities as $amenity)
+                                        <div class="col-md-2 checkbox-item">
+                                        <input type="checkbox" id="amenity_{{ $amenity->id }}" name="amenities_id[]" value="{{ $amenity->id }}"
+                                        @if(in_array($amenity->id, $selectedAmenities)) checked @endif>
+                                            <label for="amenity_{{ $amenity->id }}">{{ $amenity->name }}</label>
+                                        </div>
+                                    @endforeach
+                                    </div>
+                                </fieldset>
+                            </div>
+                            
+                            
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="row formtype">
                             <div class="col-md-12">
                                 <div class="form-group">
                                     <label>Description</label>
@@ -136,55 +163,56 @@
                                     @enderror
                                 </div>
                             </div>
-                        </div>
+                            
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-primary buttonedit1">Create New Amenities</button>
                 </div>
+                <button type="submit" class="btn btn-primary buttonedit1">Create New Amenities</button>
             </form>
         </div>
     </div>
-    @section('script')
-    <script>
-        $(document).ready(function() {
-            // Toggle the extra bed textbox based on the checkbox state on page load
-            if ($('#extra_bed_checkbox').is(':checked')) {
+   
+@section('script')
+<script>
+    $(document).ready(function() {
+         if ($('#extra_bed_checkbox').is(':checked')) {
+            $('#extra_bed_textbox').show();
+        }
+        $('#extra_bed_checkbox').change(function() {
+            if ($(this).is(':checked')) {
                 $('#extra_bed_textbox').show();
+            } else {
+                $('#extra_bed_textbox').hide();
             }
-            $('#extra_bed_checkbox').change(function() {
-                if ($(this).is(':checked')) {
-                    $('#extra_bed_textbox').show();
-                } else {
-                    $('#extra_bed_textbox').hide();
-                }
-            });
         });
-    </script>
+        
+    });
+</script> 
 
-    <script>
-        // Calculate  per_extra_bed_price  *  extra_bed_quantity
+<script>
+    // Calculate  per_extra_bed_price  *  extra_bed_quantity
 
-        document.addEventListener('DOMContentLoaded', function () {
-            function calculateTotalExtraBedPrice() {
-                var perExtraBedPrice = parseFloat(document.getElementById('per_extra_bed_price').value) || 0;
-                var extraBedQuantity = parseInt(document.getElementById('extra_bed_quantity').value) || 0;
-                var base_price = parseInt(document.getElementById('base_price').value) || 0;
+    document.addEventListener('DOMContentLoaded', function () {
+        function calculateTotalExtraBedPrice() {
+            var perExtraBedPrice = parseFloat(document.getElementById('per_extra_bed_price').value) || 0;
+            var extraBedQuantity = parseInt(document.getElementById('extra_bed_quantity').value) || 0;
+            var base_price = parseInt(document.getElementById('base_price').value) || 0;
 
-                var totalExtraBedPrice = perExtraBedPrice * extraBedQuantity;
-                var totalBasePrice = totalExtraBedPrice + base_price;
+            var totalExtraBedPrice = perExtraBedPrice * extraBedQuantity;
+            var totalBasePrice = totalExtraBedPrice + base_price;
 
-                document.getElementById('total_extra_bed_price').textContent = totalExtraBedPrice.toFixed(2); // Update the displayed total price
-                document.getElementById('total_base_price').value = totalBasePrice.toFixed(2); // Update the displayed total price
-            }
-            document.getElementById('per_extra_bed_price').addEventListener('input', calculateTotalExtraBedPrice);
-            document.getElementById('extra_bed_quantity').addEventListener('input', calculateTotalExtraBedPrice);
-            document.getElementById('base_price').addEventListener('input', calculateTotalExtraBedPrice);
+            document.getElementById('total_extra_bed_price').textContent = totalExtraBedPrice.toFixed(2); // Update the displayed total price
+            document.getElementById('total_base_price').value = totalBasePrice.toFixed(2); // Update the displayed total price
+        }
+        document.getElementById('per_extra_bed_price').addEventListener('input', calculateTotalExtraBedPrice);
+        document.getElementById('extra_bed_quantity').addEventListener('input', calculateTotalExtraBedPrice);
+        document.getElementById('base_price').addEventListener('input', calculateTotalExtraBedPrice);
 
-            calculateTotalExtraBedPrice();
-        });
-    </script>
+        calculateTotalExtraBedPrice();
+    });
+</script>
 
 
-    @endsection
+@endsection
 
 @endsection
