091b95b2ea10664a3de6b16d97cc6a22a4e613e7
Dhruvish <165013408+drashtipatell18@users.noreply.github.com>
2024-10-11 16:28:39 +0530

    Resend OTP And Create Password Route

--- routes/web.php
+++ routes/web.php
@@ -387,3 +387,7 @@ Route::post('/logoutfrontend', [IndexController::class, 'logoutfrontend'])->name
 Route::post('/loginstore', [IndexController::class, 'authenticate'])->name('login.authenticate');
 Route::post('/forget-password', [IndexController::class, 'forgotPassword'])->name('forget.password');
 Route::post('/verify-otp', [IndexController::class, 'verifyOtp'])->name('verify.otp');
+
+
+Route::post('/password/reset', [IndexController::class, 'resetPassword'])->name('password.reset');
+Route::post('/resend-otp', [IndexController::class, 'resendOtp'])->name('resend.otp');
